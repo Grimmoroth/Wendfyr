d428c44858c47c88cf4aaa98d89e5b8df9f8d560
Grimmoroth <grimmoroth@gmail.com>
2026-03-31 23:38:45 +0200

    Write ICommand interface

---
+++ libs/core/include/wendfyr/domain/commands/i_command.hpp
@@ -0,0 +1,25 @@
+#ifndef WENDFYR_DOMAIN_COMMANDS_I_COMMAND_HPP
+#define WENDFYR_DOMAIN_COMMANDS_I_COMMAND_HPP
+
+#include <string>
+
+namespace wendfyr::domain::commands
+{
+    class ICommand
+    {
+      public:
+        virtual ~ICommand() = default;
+        virtual void execute() = 0;
+        virtual void undo() = 0;
+        [[nodiscard]] virtual std::string description() const = 0;
+
+      protected:
+        ICommand() = default;
+        ICommand(const ICommand&) = default;
+        ICommand(ICommand&&) = default;
+        ICommand& operator=(const ICommand&) = default;
+        ICommand& operator=(ICommand&&) = default;
+    };
+};  // namespace wendfyr::domain::commands
+
+#endif
