966e3cbf90ec5c6c12b2b76d56fb66fd73452020
Grimmoroth <grimmoroth@gmail.com>
2026-04-08 08:32:37 +0200

    Added MoveCommand.hpp

---
+++ libs/core/src/domain/commands/move_command.hpp
@@ -0,0 +1,41 @@
+#ifndef WENDFYR_DOMAIN_COMMANDS_MOVE_COMMAND_HPP
+#define WENDFYR_DOMAIN_COMMANDS_MOVE_COMMAND_HPP
+
+#include <filesystem>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace wendfyr::ports::driven
+{
+    class IFilesystemService;
+};
+
+namespace wendfyr::services
+{
+    class EventBus;
+};
+
+namespace wendfyr::domain::commands
+{
+    class MoveCommand final : public ICommand
+    {
+      public:
+        MoveCommand(std::filesystem::path sources, std::filesystem::path destination,
+                    ports::driven::IFilesystemService& fs, services::EventBus& event_bus);
+
+        void execute() override;
+        void undo() override;
+        [[nodiscard]] std::string description() const override;
+
+      private:
+        std::vector<std::filesystem::path> _sources;
+        std::filesystem::path _destination;
+        std::vector<std::pair<std::filesystem::path, std::filesystem::path>> _moved_redords;
+
+        ports::driven::IFilesystemService& _fs;
+        services::EventBus& event_bus;
+    };
+};  // namespace wendfyr::domain::commands
+
+#endif
