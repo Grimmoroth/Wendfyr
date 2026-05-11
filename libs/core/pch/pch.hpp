97a1f1a7a622d3f5d0f7c6f3134423843fcdb762
Grimmoroth <grimmoroth@gmail.com>
2026-05-11 12:49:09 +0200

    Refactor: Add precompiled header mainly for filesystem

---
+++ libs/core/pch/pch.hpp
@@ -0,0 +1,19 @@
+#ifndef WENDFYR_PCH_HPP
+#define WENDFYR_PCH_HPP
+
+#include <spdlog/spdlog.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <span>
+#include <string>
+#include <variant>
+#include <vector>
+
+#endif
